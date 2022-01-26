d8740a1d5c0b273e263e9cff378050556ecfce85
WakelessSloth56 <wsloth56@foxmail.com>
2022-01-26 15:50:07 +0800

    fix: output only trim end

--- src/handler.ts
+++ src/handler.ts
@@ -11,7 +11,7 @@ let s: string[] = [];
 function output(): void {
     process.stdout.write('\x1b[H\x1b[J'); // move cursor to top left corner and clear screen
 
-    console.info(s.join('\n').trim());
+    console.info(s.join('\n').trimEnd());
     console.info(`Polling cycle ${cycle}, time: ${isoDate(Date.now())}`);
 
     i = 0;
